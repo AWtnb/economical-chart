a75a25cf77ba66534b695555663e2b1713b1ee91
AWtnb <arikaebanataw@gmail.com>
2021-02-21 23:54:39 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+[Chart.js](https://www.chartjs.org/) を使って各種グラフを描画してみる練習。
---
+++ script.js
@@ -0,0 +1,103 @@
+// https://www.chartjs.org/samples/latest/
+
+const xs = [];
+for(let i=0; i<=200; i++) { xs.push(i*0.1); }
+
+function calc_supply(xs, deltax) {
+  return xs.map(x => 2*x + -1*deltax).map(y => {
+    if (10<=y && y<=30) {
+      return y;
+    }
+    return null;
+  });
+}
+
+function calc_demand(xs, deltax) {
+  return xs.map(x => -2*x + 40 + deltax).map(y => {
+    if (10<=y && y<=30) {
+      return y;
+    }
+    return null;
+  });
+}
+
+const ctx = document.getElementById("myChart").getContext('2d');
+
+let myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: xs,
+    datasets: [
+      {
+      fill: false,
+      borderColor: "#ff1267",
+      backgroundColor: "#ff126788",
+      label: '供給曲線',
+      data: calc_supply(xs, 0),
+      pointRadius: 0,
+    },
+    {
+      fill: false,
+      borderColor: "#3300ee",
+      backgroundColor: "#3300ee88",
+      label: '需要曲線',
+      data: calc_demand(xs, 0),
+      pointRadius: 0,
+    }
+  ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      position: "top",
+      labels: {
+        boxWidth: 20,
+        padding: 4
+      }
+    },
+    tooltips: {
+      enabled: false
+    },
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false,
+          drawBorder: true,
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Quantity',
+        },
+        ticks: {
+          display: false,
+          min:0, max:40
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          display: true,
+          drawBorder: true,
+        },
+        scaleLabel: {
+          display: true,
+          labelString: "Price",
+        },
+        ticks: {
+          min:0, max:40,
+          display: false,
+        }
+      }]
+    }
+  }
+});
+
+var sliders = document.getElementById("sliders");
+sliders.addEventListener("input", function() {
+  const s = document.getElementById("slider_supply").value;
+  const d = document.getElementById("slider_demand").value;
+  myChart.data.datasets[0].data = calc_supply(xs, Number(s));
+  myChart.data.datasets[1].data = calc_demand(xs, Number(d));
+  const clossX = 10 + 1/4*(Number(s) + Number(d));
+  myChart.update();
+}, false);
